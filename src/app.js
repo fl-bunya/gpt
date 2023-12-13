4fde7c915cf9884a40b1503a4c8835dea013175a
fl-bunya <yuichi.bunya@firstlogic.co.jp>
2023-12-13 22:07:48 +0900

    Add function to check if a string contains many emojis

--- src/app.js
+++ src/app.js
@@ -19,6 +19,22 @@ module.exports.handler = async (event, context, callback) => {
   return handler(event, context, callback);
 };
 
+function containsManyEmojis(str) {
+  const SHRESHOLD = 3;
+  
+  const unicodeRegex = /[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{1F700}-\u{1F77F}\u{1F780}-\u{1F7FF}\u{1F800}-\u{1F8FF}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FA6F}\u{1FA70}-\u{1FAFF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}\u{1F1E6}-\u{1F1FF}]/gu;
+  const unicodeEmojis = str.match(unicodeRegex);
+  const unicodeCount = unicodeEmojis ? unicodeEmojis.length : 0;
+
+  const slackRegex = /:[a-zA-Z0-9_+-]+:/g;
+  const slackEmojis = str.match(slackRegex);
+  const slackCount = slackEmojis ? slackEmojis.length : 0;
+
+  const emojiCount = unicodeCount + slackCount;
+  console.log('%capp.js line:34 emojiCount', 'color: #007acc;', emojiCount);
+  return emojiCount >= SHRESHOLD;
+}
+
 app.event("app_mention", async ({ event, say }) => {
   try {
     const { text, channel, thread_ts, ts } = event;
@@ -41,7 +57,7 @@ app.event("app_mention", async ({ event, say }) => {
     await say({
       text: newReply,
       thread_ts: thread_ts || ts,
-      icon_emoji: ':smiley:',
+      icon_emoji: containsManyEmojis(newReply) ? ":kissing_heart:" : null,
     });
   } catch (error) {
     console.error("Error:", error);
